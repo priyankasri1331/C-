b62dc554846175016181094643182418113a0df4
Priyanka K <priyankasri1331@gmail.com>
2021-08-31 14:13:29 -0500

    Update Linked_list_rev.cpp

--- Leetcode_Probs/Recursion/Linked_list_rev.cpp
+++ Leetcode_Probs/Recursion/Linked_list_rev.cpp
@@ -13,3 +13,20 @@
         
         return prev_ptr;
     }
+
+
+class Solution {
+public:
+    
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head == nullptr || head->next == nullptr)
+            return head;
+        
+        ListNode* p = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        
+        return p;
+    }
+};
